19746aac05e8a7d4911098891202547ccff6e4ad
Alao Oluwaseyin <seyinalao@gmail.com>
2025-01-30 15:31:29 +0100

    Added UTC

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function updateTime() {
     const now = new Date();
     const utcTime = now.toUTCString().split(' ')[4];
   
-    document.getElementById('current-time').innerText = utcTime;
+    document.getElementById('current-time').innerText = `${utcTime} UTC`;
 }
 updateTime();
 setInterval(updateTime, 1000);
